edb8a7a990ddb88e909fd48390334b1eacb40771
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2018-05-05 15:48:57 -0300

    refactor

---
+++ corebot/corebot.py
@@ -0,0 +1,69 @@
+from corebot.simple_messenger import SimpleHtmlMessenger
+from corebot.db import MongoCore
+from corebot.requests_jarbas import Requester
+import logging
+
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+class CoreBot:
+
+    def __init__(self, messenger=None, db=None, requester=None):
+
+        self.messenger = messenger if messenger else SimpleHtmlMessenger()
+        self.db = db if db else MongoCore()
+        self.requester = requester if requester else Requester()
+
+    def execute_command(self, command, user_id):
+        args = self._translate_command(command)
+        return self._execute(user_id, args)
+
+    def _translate_command(self, command):
+        if command[:5] == "/nome":
+            return [command[:5], " ".join(command.split(' ')[1:])]
+        else:
+            return [command.split("_")[0], command.split("_")[1:]]
+
+    def _execute(self, user_id, command):
+        command_list = {
+            "/nome": self._command_name,
+            "/ajuda": self._command_help,
+            "/lista": self._command_list,
+            "/seguir": self._command_follow,
+            "/deixardeseguir": self._command_unfollow,
+            "/start": self._command_start,
+            "/historico": self._command_history
+        }
+
+        return command_list[command[0]](user_id, command)
+
+    def _command_start(self, user_id, command):
+        self.db.insert_user(user_id)
+        return self.messenger.start_message()
+
+    def _command_help(self, user_id, command):
+        return self.messenger.help_message()
+
+    def _command_list(self, user_id, command):
+        itens = [[self.db.get_congressperson_name(x['id']), x['id']] for x in self.db.get_user_following(user_id)]
+        return self.messenger.user_user_following(itens)
+
+    def _command_follow(self, user_id, command):
+        name = self.db.add_congressperson_to_follow(user_id, int(command[1][0]))
+        return self.messenger.follow_congressperson(name, command[1][0])
+
+    def _command_unfollow(self, user_id, command):
+        name = self.db.remove_congressperson_to_follow(user_id, int(command[1][0]))
+        return self.messenger.unfollow_congressperson(name)
+
+    def _command_name(self, user_id, command):
+        itens = self.requester.find_names(command[1])
+        return self.messenger.names_list(command[1], itens)
+
+    def _command_history(self, user_id, command):
+        itens = self.requester.find_suspicions(command[1][0])
+        name = self.db.get_congressperson_name(command[1][0])
+        see_more_command = ""
+        return self.messenger.history_message(itens, see_more_command)
---
+++ corebot/requests_jarbas.py
@@ -0,0 +1,39 @@
+import os
+import requests
+import itertools
+import logging
+
+from urllib.parse import quote
+
+from corebot.db import MongoCore
+
+
+class Requester:
+
+    def __init__(self, db=None):
+
+        self.HOST = os.environ.get("JARBAS_HOST")
+        self.mongo = db if db else MongoCore()
+
+    def find_names(self, name):
+
+        url = '%s?search=%s' % (self.HOST, quote(name))
+
+        response = requests.get(url)
+
+        b = [[x['congressperson_name'], x['applicant_id']] for x in response.json()['results']]
+        b.sort()
+        names = list(b for b, _ in itertools.groupby(b))
+        for x in names:
+            logging.info('Adicionando politico: %s - %s', x[0], x[1])
+            self.mongo.add_congressperson_to_list(x[0], x[1])
+        return names
+
+    def find_suspicions(self, id):
+
+        url = '%s?suspicions=1&applicant_id=%s' % (self.HOST, id)
+
+        response = requests.get(url)
+        b = [x for x in response.json()['results']]
+
+        return b
